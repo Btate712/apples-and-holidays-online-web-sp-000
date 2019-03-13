a337bbf5c40d58864378c8bd4d838f09fe45fb25
Robert Tate <btate712@gmail.com>
2019-03-13 00:21:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,7 +19,6 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # }
   # return the second element in the 4th of July array
   holiday_hash.each do |season, holidays|
-    binding.pry
     holidays.each do |holiday, supplies|
       if holiday == :fourth_of_july
         return supplies[1]
