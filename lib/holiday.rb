064b7603320387e7f19c4b15b72a83dc36a61aed
Robert Tate <btate712@gmail.com>
2019-03-13 00:34:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # holiday_hash.each do |season_key, holidays|
     # if season_key == season
   holiday_hash[season][holiday_name] = supply_array
-    
+
 end
 
 def all_winter_holiday_supplies(holiday_hash)
