67decf8beb43ac0ccdbe7cbe73ab2385b91771a8
Robert Tate <btate712@gmail.com>
2019-03-13 00:39:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,12 +83,12 @@ def all_supplies_in_holidays(holiday_hash)
     holidays.each do |holiday, supplies|
     end
   end
-      
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
-      
+
 end
