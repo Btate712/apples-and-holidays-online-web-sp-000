f95e3ef066dda91a20d54fae15476c8c87f26409
Robert Tate <btate712@gmail.com>
2019-03-13 00:49:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,10 +78,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    binding.pry
     puts "#{season.to_s.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts
+      print "  #{holiday.to_s.capitalize}: "
+      binding.pry
+      supplies.join(", ")
     end
   end
 
